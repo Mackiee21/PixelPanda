c3b2e29029fa2bfa60853d62ec98f7c0b158e871
Mackiee21 <markpandac02@gmail.com>
2023-06-21 19:08:23 +0800

    added lazy loading

--- client/src/components/Photos.jsx
+++ client/src/components/Photos.jsx
@@ -1,7 +1,7 @@
 import PageHeader from "./pageHeader";
 import { createPortal } from 'react-dom';
 import Modal from './Modal';
-import { useState, useRef, forwardRef } from 'react';
+import { useEffect, useRef, forwardRef } from 'react';
 import '../scss/photos.scss';
 import useMainRefContext from '../customHooks/useMainRefContext';
 import photo1 from '../assets/Web/chosen1.jpg';
@@ -16,10 +16,10 @@ import photo9 from '../assets/Web/chosen9.jpg';
 
 
 function Photos(_, ref) {
-    const { galleryContext } = useMainRefContext();
-    console.log("gallery context at photos", galleryContext)
-    const [showModal, setShowModal] = useState(false);
-    const [currEl, setCurrEl] = useState(null);
+    const imageRef = useRef(null);
+    // const { galleryContext } = useMainRefContext();
+    // const [showModal, setShowModal] = useState(false);
+    // const [currEl, setCurrEl] = useState(null);
 
     const pageHeaderInfo = {
         link: "https://scontent.fceb1-2.fna.fbcdn.net/v/t39.30808-6/348436373_213259914852842_6357758812729540911_n.jpg?_nc_cat=100&cb=99be929b-59f725be&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeH60lnrXeDshehXAEKwLStxGvpQaazAut4a-lBprMC63jIwRFw0pA4wtS0Zf12wWl4QnDYNs6FGmNMOrrLK7tJT&_nc_ohc=4CG7Ju-xwl8AX-8V8AY&_nc_ht=scontent.fceb1-2.fna&oh=00_AfBw2tdB3YV2kfI8R3tLv28xhREekxYM5Ih08bGnSQKFew&oe=6493E170",
@@ -32,6 +32,22 @@ function Photos(_, ref) {
         setCurrEl(cards[index]);
         setShowModal(true);
     }
+
+    useEffect(() => {
+        const divs = imageRef.current.children;
+        Array.from(divs).forEach(div => {
+            const img = div.querySelector("img");
+
+            function loaded() {
+                div.classList.add("loaded");
+            }
+            if(img.complete){
+                loaded();
+            }else{
+                img.addEventListener("load", loaded)
+            }
+        })
+    }, []);
 
     // const closeModal = () => {
     //     document.body.style.overflowY = "scroll";
@@ -53,7 +69,7 @@ function Photos(_, ref) {
     return(
         <div ref={ref} className="wrapper" id="1">
             <PageHeader pageHeaderInfo={pageHeaderInfo} />
-           <div className="grid">
+           <div className="grid" ref={imageRef}>
             { images }
            </div>
            <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.pexels.com%2F%40pixelpanda-1386402%2F%3Fnc%3D%26fbclid%3DIwAR0H_24xSRCxXcRy7-CRb16vrAha_GUVpkh3uMbnCeoGafYNtN5QGK631iU&h=AT0y6o-3Wx-R-I6t7UCHpbdOSknAE-9_3iienpdwNX-i90w3jo4i8Gyn0MZrTpWOTuyBlJ-Y76JF-3ZhAWiwSQZLEh9tVlvet0PPgmlVZS8tnHY8n8aBjcQ0QsU0cvxa4iwEswDSseDdHUWoSOE7gA" target="_blank" className="link">View More <i className="fa-solid fa-arrow-right"></i></a>
